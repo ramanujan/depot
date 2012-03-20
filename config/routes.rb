5dedd74cabb2433789c69597af67db9db5c7ff63
Ramanujan Jan <esis_coder@libero.it>
2012-03-20 10:48:57 +0100

    Heroku-Fastweb workaround attempt two

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Depot::Application.routes.draw do
   get '/home', :to=> 'static_pages#home' 
   get '/about', to:"static_pages#about"
   get '/contact', to:"static_pages#contact"
-  
+  get '/heroku_reset', to:"static_pages#heroku_db_reset"
+  get '/heroku_migrate', to:"static_pages#heroku_db_migrate"
   resources :products
    
  
